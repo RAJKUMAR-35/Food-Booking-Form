6bc8d313a4ee40f5a3e17eac07c3682c6dd077ff
RAJKUmar-35 <rajkumaranbu192@gmail.com>
2024-06-27 23:15:00 +0530

    Bug fixed

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 var subBut = document.getElementById("submit");
-// console.log(subBut);
+
 subBut.addEventListener("click", (e)=>{
     e.preventDefault();
-   // Getting input text values
+
    let firstName = document.getElementById('first-name').value;
    let lastName = document.getElementById('last-name').value;
    let address = document.getElementById('address').value;
    let pincode = document.getElementById('pincode').value;
    let state = document.getElementById('formState').value;
    let country = document.getElementById('formCountry').value;
-  //  console.log(firstName, lastName, address, pincode, state, country);
-
-    // Getting radio box value
+  
     const genderInputs = document.getElementsByName('gender');
     let gender = '';
     for (let GI of genderInputs) {
@@ -20,11 +18,7 @@ subBut.addEventListener("click", (e)=>{
         break;
       }
     }
-    // console.log(gender);
-
-  // Getting check box values
   const foodInputs = document.getElementsByName('foodCheck');
-  // console.log(foodInputs);
   let selectedFood = [];
   for (let FI of foodInputs) {
     if (FI.checked) {
@@ -37,7 +31,6 @@ subBut.addEventListener("click", (e)=>{
     selectedFood = selectedFood.join(', ');
     // console.log(selectedFood);
   
-        // Creating table dynamic row
         let tBody = document.getElementById('tBody');
         let newR = document.createElement('tr');
         newR.innerHTML = ` 
@@ -51,11 +44,9 @@ subBut.addEventListener("click", (e)=>{
           <td>${selectedFood}</td>
         `;
         
-        // adding new row
         tBody.append(newR);
         alert(`Your Opinion Successfully Added!`);
   
-        // Clearing form fields
         document.getElementById('form').reset();
   }
   
